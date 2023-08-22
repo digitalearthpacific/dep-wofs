6e38249275b129cfdc151263cb62004e37c704e4
Jesse Anderson <jesse.anderson@gmail.com>
2023-08-22 16:55:56 +1200

    fixed grid url

--- src/grid.py
+++ src/grid.py
@@ -1,7 +1,9 @@
 import geopandas as gpd
 
 grid = (
-    gpd.read_file("../dep-grid/grid_pacific.geojson")
+    gpd.read_file(
+        "https://raw.githubusercontent.com/digitalearthpacific/dep-grid/master/grid_pacific.geojson"
+    )
     .astype({"code": str, "gid": str})
     .set_index(["code", "gid"], drop=False)
 )
