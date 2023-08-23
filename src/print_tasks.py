764211b25ac817dfb6885e079c9008d78662b3ad
Jesse Anderson <jesse.anderson@gmail.com>
2023-08-23 12:02:13 +1200

    reworked print_tasks.py

--- src/print_tasks.py
+++ src/print_tasks.py
@@ -1,15 +1,28 @@
 from itertools import product
+from typing import List, Union, Annotated
 
 import typer
 from azure_logger import CsvLogger, filter_by_log, get_log_path
 from dep_tools.utils import get_container_client
 from grid import grid
 
+#    region_codes: Annotated[List, typer.Option()],
+#    dataset_id: str = "wofs",
+#    version: str = "21Aug2023",
+#    years=2023
+#    limit: int = None,
+
 
 def main(
-    dataset_id: str = "wofs", version: str = "21Aug2023", years=range(2013, 2024)
+    region_code: Annotated[str, typer.Option()],
+    datetime: Annotated[str, typer.Option()],
+    version: Annotated[str, typer.Option()],
+    limit: Annotated[str, typer.Option()],
+    dataset_id: str = "wofs",
 ) -> None:
-    aoi_by_tile = grid
+    grid_subset = (
+        grid.loc[grid.code == region_code] if region_code is not None else grid
+    )
 
     prefix = f"{dataset_id}/{version}"
     logger = CsvLogger(
@@ -20,8 +33,13 @@ def main(
         header="time|index|status|paths|comment\n",
     )
 
-    aoi_by_tile = filter_by_log(aoi_by_tile, logger.parse_log())
-    print(list(product(aoi_by_tile.index, years)))
+    filter_by_log(grid_subset, logger.parse_log())
+    params = [
+        {"region_code": l[0][0], "region_id": l[0][1], "datetime": l[1]}
+        for l in product(grid_subset.index, [datetime])
+    ]
+
+    print(params[0 : int(limit)] if limit is not None else params)
 
 
 if __name__ == "__main__":
