fd826eb94794848388b9378ae2a2b405a8fe7207
Jesse Anderson <jesse.anderson@gmail.com>
2024-08-16 16:07:16 -0700

    Full implementation, but the output looks terrible

--- dep_wofs/run_task.py
+++ dep_wofs/run_task.py
@@ -1,89 +1,80 @@
 from typing_extensions import Annotated
 
 from distributed import Client
-from odc.stac import configure_s3_access
-import planetary_computer as pc
+from wofs.virtualproduct import scale_usgs_collection2
+from wofs.wofls import woffles_usgs_c2
+from odc.stac import configure_s3_access, load
+from odc.stats.plugins.wofs import StatsWofs
 from typer import Option, run
-from xarray import DataArray, Dataset
+from xarray import Dataset
 
 from cloud_logger import CsvLogger, S3Handler
 from dep_tools.loaders import OdcLoader
 from dep_tools.namers import S3ItemPath
 from dep_tools.processors import LandsatProcessor, XrPostProcessor
-from dep_tools.searchers import LandsatPystacSearcher
+from dep_tools.searchers import LandsatPystacSearcher, PystacSearcher
 from dep_tools.task import AwsStacTask as Task
-from dep_tools.utils import scale_and_offset
 from grid import grid
 
 
-def wofs(tm: Dataset) -> DataArray:
-    # First, rescale to what the wofs model expects
-    # (input values should be scaled, not raw int)
-    l1_scale = 0.0001
-    l1_rescale = 1.0 / l1_scale
-    tm = scale_and_offset(tm, scale=[l1_rescale])
-    # lX indicates a left path from node X
-    # rX indicates a right
-    # dX is just the logic for _that_ node
-    tm["ndi52"] = normalized_ratio(tm.swir16, tm.green)
-    tm["ndi43"] = normalized_ratio(tm.nir08, tm.red)
-    tm["ndi72"] = normalized_ratio(tm.swir22, tm.green)
-
-    d1 = tm.ndi52 <= -0.01
-    l2 = d1 & (tm.blue <= 2083.5)
-    d3 = tm.swir22 <= 323.5
-
-    l3 = l2 & d3
-    w1 = l3 & (tm.ndi43 <= 0.61)
-
-    r3 = l2 & ~d3
-    d5 = tm.blue <= 1400.5
-    d6 = tm.ndi72 <= -0.23
-    d7 = tm.ndi43 <= 0.22
-    w2 = r3 & d5 & d6 & d7
-
-    w3 = r3 & d5 & d6 & ~d7 & (tm.blue <= 473.0)
-
-    w4 = r3 & d5 & ~d6 & (tm.blue <= 379.0)
-    w7 = r3 & ~d5 & (tm.ndi43 <= -0.01)
-
-    d11 = tm.ndi52 <= 0.23
-    l13 = ~d1 & d11 & (tm.blue <= 334.5) & (tm.ndi43 <= 0.54)
-    d14 = tm.ndi52 <= -0.12
-
-    w5 = l13 & d14
-    r14 = l13 & ~d14
-    d15 = tm.red <= 364.5
-
-    w6 = r14 & d15 & (tm.blue <= 129.5)
-    w8 = r14 & ~d15 & (tm.blue <= 300.5)
-
-    w10 = (
-        ~d1
-        & ~d11
-        & (tm.ndi52 <= 0.32)
-        & (tm.blue <= 249.5)
-        & (tm.ndi43 <= 0.45)
-        & (tm.red <= 364.5)
-        & (tm.blue <= 129.5)
-    )
-
-    water = w1 | w2 | w3 | w4 | w5 | w6 | w7 | w8 | w10
-    return water.where(tm.red.notnull())
-
-
-def normalized_ratio(band1: DataArray, band2: DataArray) -> DataArray:
-    return (band1 - band2) / (band1 + band2)
+SR_BANDS = ["blue", "green", "red", "nir08", "swir16", "swir22"]
 
 
 class WofsLandsatProcessor(LandsatProcessor):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-    def process(self, xr: Dataset) -> Dataset:
-        xr = super().process(xr)
-        output = wofs(xr).resample(time="1Y").mean().squeeze()
-        return output.to_dataset(name="mean", promote_attrs=True)
+    def process(self, ds: Dataset) -> Dataset:
+        ds = super().process(ds)
+
+        woffles = dea_wofls(ds)
+        summarizer = StatsWofs()
+        prepped = woffles.groupby("time").apply(
+            lambda da: summarizer.native_transform(da.to_dataset(name="water"))
+        )
+        return summarizer.reduce(prepped)[["frequency"]]
+
+
+def dea_wofls(ds: Dataset):
+    ds[SR_BANDS] = scale_usgs_collection2(ds[SR_BANDS])
+    # rename for wofs functionality
+    ds = ds.rename(
+        {
+            "blue": "nbart_blue",
+            "green": "nbart_green",
+            "red": "nbart_red",
+            "nir08": "nbart_nir",
+            "swir16": "nbart_swir_1",
+            "swir22": "nbart_swir_2",
+            "qa_pixel": "fmask",
+        }
+    )
+    # computing here so it doesn't need to be reloaded for each time step
+    dsm = load_dsm(ds).compute()
+
+    return ds.groupby("time").apply(
+        # There should be no nodata in the cop data, but we need to think about
+        # tile margins (all land should be covered) and see the odc stac issue
+        # about gaps
+        lambda ds_yr: woffles_usgs_c2(ds_yr.squeeze(), dsm, ignore_dsm_no_data=True)
+    )
+
+
+def load_dsm(like: Dataset):
+    # Use this instead of just searching to be OK across -180
+    items = PystacSearcher(
+        catalog="https://earth-search.aws.element84.com/v1",
+        collections=["cop-dem-glo-30"],
+    ).search(like.odc.geobox)
+
+    return (
+        load(
+            items, like=like, chunks=dict(x=like.chunks["x"][0], y=like.chunks["y"][0])
+        )
+        .rename(dict(data="elevation"))  # renamed for wofs functionality
+        .squeeze()
+        .assign_attrs(crs=like.rio.crs)  # needed for wofs functionality
+    )
 
 
 def bool_parser(raw: str):
@@ -126,17 +117,17 @@ def main(
     )
 
     stacloader = OdcLoader(
-        clip_to_area=True,
-        epsg=cell.crs,
-        dtype="float32",
-        bands=["red", "green", "blue", "nir08", "swir16", "swir22", "qa_pixel"],
+        # clip_to_area=True,
+        groupby="solar_day",  # OK but we may need to revisit for clouds
+        crs=cell.crs,
+        dtype="uint16",
+        bands=SR_BANDS + ["qa_pixel"],
         chunks=dict(band=1, time=1, x=4096, y=4096),
         fail_on_error=False,
         resolution=30,
-        #        patch_url=pc.sign,
     )
 
-    processor = WofsLandsatProcessor(mask_clouds_kwargs=dict(filters=[("dilation", 2)]))
+    processor = WofsLandsatProcessor(mask_clouds=False, scale_and_offset=False)
     post_processor = XrPostProcessor(
         convert_to_int16=True,
         output_value_multiplier=100,
