90acdf8150b8ded51e5e926888519ef86d010cb7
Jesse Anderson <jesse.anderson@gmail.com>
2025-04-18 16:19:38 -0700

    cleanup

--- validation/sample_wofls_images.py
+++ validation/sample_wofls_images.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from random import sample, seed
 import warnings
 import xml.etree.ElementTree as ET
@@ -12,24 +13,32 @@ import pandas as pd
 from pystac_client import Client
 import rioxarray
 from shapely.geometry import Point
-import xarray as xr
 
 from dep_wofs.grid import ls_grid
 from dep_wofs.utils import use_alternate_s3_href
 
 from summarize_wofl_tiles import get_ocean_and_land_classes
 
-CLIENT = Client.open("https://stac.digitalearthpacific.org")
+DEP_CLIENT = Client.open("https://stac.digitalearthpacific.org")
+OUTPUT_DIR = Path("data/validation")
+
+
+def sample_catalog(pathrows, query=dict(), n_each=1):
+    return [sample_tile(path, row, query, n_each)[0] for path, row in pathrows]
 
 
 def sample_tile(path, row, query=dict(), n=1):
+    """For the given path and row and other query parameters, return n randomly
+    selected STAC Items."""
     tile_bbox = ls_grid.loc[(path, row)].geometry.bounds
     query = {
         "landsat:wrs_row": dict(eq=str(row).zfill(3)),
         "landsat:wrs_path": dict(eq=str(path).zfill(3)),
     } | query
     items = list(
-        CLIENT.search(collections=["dep_ls_wofl"], bbox=tile_bbox, query=query).items()
+        DEP_CLIENT.search(
+            collections=["dep_ls_wofl"], bbox=tile_bbox, query=query
+        ).items()
     )
     if n > len(items):
         n = len(items)
@@ -40,31 +49,50 @@ def sample_tile(path, row, query=dict(), n=1):
     return sample(items, n)
 
 
-def sample_catalog(query=dict()):
-    path_rows = [(99, 63), (81, 71), (100, 65), (81, 75), (75, 66)]
-    return [sample_tile(path, row, query)[0] for path, row in path_rows]
+def place_random_points(item, n=25):
+    """Load the "dry" and "wet" wofl classes for the given item, and sample
+    n points within each zone."""
+    wofl = odc.stac.load([item]).squeeze(drop=True)  # drop time
+    classes_to_sample = get_classes_to_sample(wofl)
+    counts = get_counts(classes_to_sample, n)
+    points = pd.concat(
+        [
+            sample_bool_da(classes_to_sample[var], count).assign(name=var)
+            for var, count in counts.items()
+        ]
+    )
 
-
-def sample_da(da, n):
-    # I think it's still possible we have dupes
-    pts = pd.DataFrame(
-        dict(
-            x=np.random.choice(da.x.values, size=n),
-            y=np.random.choice(da.y.values, size=n),
-        )
-    ).to_xarray()
-    df = da.sel(pts, method="nearest").to_dataframe()
-    return gpd.GeoDataFrame(
-        df, geometry=df[["x", "y"]].apply(Point, axis=1), crs=da.odc.crs
+    points.to_file(OUTPUT_DIR / f'{item.properties["landsat:scene_id"]}.gpkg')
+    points.drop(["name"], axis=1).assign(label="").sample(frac=1).to_file(
+        OUTPUT_DIR / f'{item.properties["landsat:scene_id"]}_blinded.gpkg'
     )
 
 
-def min_distance_to_series(point, point_series):
-    dists = point.distance(point_series)
-    return min(dists[dists > 0])
+def get_classes_to_sample(wofl):
+    # This call returns a 3 variable Dataset with "dry", "wet", and "bad".
+    dry_and_wet = StatsWofs().native_transform(wofl).drop_vars("bad")
+
+    # Add "_land" and "_ocean" suffixes to each class
+    dry_and_wet_land_ocean_classes = get_ocean_and_land_classes(dry_and_wet)
+    # remove small areas
+    return dry_and_wet_land_ocean_classes.map(
+        mask_cleanup, mask_filters=[("erosion", 3), ("dilation", 3)]
+    )
+
+
+def get_counts(classes_to_sample, n=25):
+    return {
+        name: min(value.item(), n)
+        for name, value in classes_to_sample.sum().data_vars.items()
+    }
 
 
 def sample_bool_da(bool_da, n, minimum_spacing=250):
+    """For the given boolean type DataArray, sample n points within the `True`
+    zones. Minimum spacing ensures that all points are at least that far apart.
+    Fewer points than n may be returned if there are not enough cells with the
+    given criteria and distance.
+    """
     bool_da_stacked = bool_da.stack(z=("x", "y"))
     positives = bool_da_stacked[bool_da_stacked]
     max_samples = len(positives)
@@ -98,54 +126,18 @@ def sample_bool_da(bool_da, n, minimum_spacing=250):
     )
 
 
-def get_classes_to_sample(wofl):
-    dry_and_wet = StatsWofs().native_transform(wofl).drop_vars("bad")
-    dry_and_wet_land_ocean_classes = get_ocean_and_land_classes(dry_and_wet)
-    # remove small areas
-    return dry_and_wet_land_ocean_classes.map(
-        mask_cleanup, mask_filters=[("erosion", 3), ("dilation", 3)]
-    )
-
-
-def get_counts(classes_to_sample, n=25):
-    return {
-        name: min(value.item(), n)
-        for name, value in classes_to_sample.sum().data_vars.items()
-    }
-
-
-def place_random_points(item, n=25):
-    wofl = odc.stac.load([item]).squeeze(drop=True)  # drop time
-    classes_to_sample = get_classes_to_sample(wofl)
-    counts = get_counts(classes_to_sample, n)
-    points = pd.concat(
-        [
-            sample_bool_da(classes_to_sample[var], count).assign(name=var)
-            for var, count in counts.items()
-        ]
-    )
-
-    points.to_file(f'J{item.properties["landsat:scene_id"]}.gpkg')
-    points.drop(["name"], axis=1).assign(label="").sample(frac=1).to_file(
-        f'J{item.properties["landsat:scene_id"]}_blinded.gpkg'
-    )
-
-
-# item_id = item.properties["landsat:scene_id"]
-#    classes_to_sample.astype("int8").rio.to_raster(
-#        f"{item_id}_classes_to_sample.tif", driver="COG"
-#    )
-# wofl.rio.to_raster(f"wofl_{item.properties['landsat:scene_id']}.tif", driver="COG")
-# no_data.astype("int8").rio.to_raster(f"nodata_{item_id}.tif", driver="COG")
-# land.astype("int8").rio.to_raster(f"land_{item_id}.tif", driver="COG")
-
-# decoded_wofl.astype("int8").rio.to_raster(
-#    f"decoded_wofl_{item.properties['landsat:scene_id']}.tif", driver="COG"
-# )
+def min_distance_to_series(point, point_series):
+    """Return minimum positive distance from a point to a set of points."""
+    dists = point.distance(point_series)
+    return min(dists[dists > 0])
 
 
 def create_rgb_mosaic(item):
-    # download data
+    """Create a 3-band rgb mosaic and corresponding vrt for the Landsat item
+    identified by the metadata (specifically item.properties["landsat:scene_id"])
+    of the given STAC item.
+    """
+    # Find landsat item
     client = Client.open(
         "https://landsatlook.usgs.gov/stac-server",
         modifier=use_alternate_s3_href,
@@ -155,18 +147,23 @@ def create_rgb_mosaic(item):
         collections=["landsat-c2l2-sr"],
         query={"landsat:scene_id": {"eq": scene_id}},
     ).items()
+    tif_file = (OUTPUT_DIR / f"{scene_id}_rgb.tif",)
+    vrt_file = OUTPUT_DIR / f"{scene_id}_rgb.vrt"
+    # load item and write 3-band tiff
     odc.stac.load(
         ls_item, chunks=dict(x=2048, y=2048), bands=["red", "green", "blue"]
-    ).squeeze().rio.to_raster(f"{scene_id}_rgb.tif", driver="COG")
+    ).squeeze().rio.to_raster(tif_file, driver="COG")
 
     # Create a VRT with standard stretch that seems to look decent in most
     # situations.
     # srcNodata ensures sources are complex sources
-    BuildVRT(f"{scene_id}_rgb.vrt", f"{scene_id}_rgb.tif", srcNodata=0)
-    set_vrt_min_max(f"{scene_id}_rgb.vrt")
+    BuildVRT(vrt_file, tif_file, srcNodata=0)
+    set_vrt_min_max(vrt_file)
 
 
 def set_vrt_min_max(vrt_path):
+    """Set the 'LUT' element of each complex source band in the given path
+    to sensible values for a Landsat RGB mosaic."""
     tree = ET.parse(vrt_path)
     root = tree.getroot()
     for source in root.findall(".//ComplexSource"):
@@ -176,7 +173,8 @@ def set_vrt_min_max(vrt_path):
 
 
 def main():
-    items = sample_catalog(query={"eo:cloud_cover": {"lt": 40}})
+    pathrows = [(99, 63), (81, 71), (100, 65), (81, 75), (75, 66)]
+    items = sample_catalog(pathrows=pathrows, query={"eo:cloud_cover": {"lt": 40}})
     for item in items:
         print(item)
         place_random_points(item)
