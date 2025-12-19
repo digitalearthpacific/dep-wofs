9eb6845396fec107d1dc18bba50dddfa321f2d0a
Jesse Anderson <jesse.anderson@gmail.com>
2025-12-19 10:37:19 -0700

    fix: adjust tags in cron workflow

--- dep_wofs/config.py
+++ dep_wofs/config.py
@@ -1,8 +1,8 @@
 import os
 
 BUCKET = os.environ.get("WOFS_BUCKET", "dep-public-data")
+VERSION = "0.2.0"
 WOFL_DATASET_ID = "wofl"
 OUTPUT_COLLECTION_ROOT = os.environ.get(
     "OUTPUT_COLLECTION_ROOT", "https://stac.digitalearthpacific.org"
 )
-VERSION = "0.2.0"
